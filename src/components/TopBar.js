107b61f7d0559cf69ff5663a8c76d7ac251e0739
Laurian Gridinoc <laurian@gridinoc.name>
2020-11-23 12:03:27 +0200

    link fix for #102

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -63,7 +63,7 @@ const TopBar = ({ player, data: { items = [], skipIncrement, partialTranscript =
         <TextField aria-label="name" isQuiet flex={1} value={title} isDisabled={!item} onChange={setTitle} />
 
         <TooltipTrigger delay={0}>
-          <ActionButton isQuiet aria-label="New" onPress={() => window.open('/#/new')}>
+          <ActionButton isQuiet aria-label="New" onPress={() => window.open('#/new')}>
             <NewItem />
           </ActionButton>
           <Tooltip>Create or import project</Tooltip>
